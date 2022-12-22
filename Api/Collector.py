b52bd4c8c2b7e1d7344c88395ad7f564dde35800
Arya Ashouri <105889684+DarkCloud9000@users.noreply.github.com>
2022-12-23 02:21:36 +0330

    Update Collector.py

--- Api/Collector.py
+++ Api/Collector.py
@@ -2,3 +2,5 @@ from bs4 import BeautifulSoup
 import requests
 
 #I'm gonna use these modules to collect some information about weather and ...
+#In this case I'm just able to grab some information from websites
+#But adding python to this project could push me to learn about api
